9382f9045166fe7bd0e78dac7ae525c4b133c82f
Larissa Araujo <larissa.jvsa@gmail.com>
2022-01-13 15:47:26 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Contents from './components/Contents';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <Contents />
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/components/Contents.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import Login from '../pages/Login';
+import Search from '../pages/Search';
+import Album from '../pages/Album';
+import Favorites from '../pages/Favorites';
+import Profile from '../pages/Profile';
+import ProfileEdit from '../pages/ProfileEdit';
+import NotFound from '../pages/NotFound';
+
+export default class Contents extends Component {
+  render() {
+    return (
+      <main>
+        <Route path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="*" component={ NotFound } />
+      </main>
+    );
+  }
+}
