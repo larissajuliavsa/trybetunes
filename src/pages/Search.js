6c2f9b2a1c491e8ae7f2fd510d6a8636b5517e33
Larissa Araujo <larissa.jvsa@gmail.com>
2022-01-17 19:10:09 -0300

    requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default class Search extends Component {
   constructor() {
@@ -7,9 +11,17 @@ export default class Search extends Component {
 
     this.state = {
       name: '',
+      keepName: '',
+      isLoading: false,
+      searchList: [], // a função searchAlbumsAPI() retorna um array de objetos
     };
 
     this.inputChange = this.inputChange.bind(this);
+    this.searchBand = this.searchBand.bind(this);
+  }
+
+  componentDidMount() {
+    this.searchBand();
   }
 
   inputChange({ target }) {
@@ -20,26 +32,71 @@ export default class Search extends Component {
     });
   }
 
-  render() {
+  /*
+    A função searchBand tem como objeto de capturar o valor da chave name e enviar para a API, para que retorne um objeto referente a este valor. Após esta requisição, limpei o nome desta chave name e guardei seu valor em outra chave, na keepName para que eu utilizasse no resultado da busca.
+  */
+
+  async searchBand() {
     const { name } = this.state;
+
+    this.setState({ isLoading: true });
+
+    const getAPI = await searchAlbumsAPI(name);
+
+    this.setState({
+      name: '',
+      keepName: name,
+      isLoading: false,
+      searchList: getAPI,
+    });
+  }
+
+  render() {
+    const { name, keepName, isLoading, searchList } = this.state;
     const MIN_LENGTH = 2;
     return (
-      <div data-testid="page-search">
-        <Header />
-        <input
-          type="text"
-          name="name"
-          placeholder="eg Rolling Stones or Jack White"
-          onChange={ this.inputChange }
-          data-testid="search-artist-input"
-        />
-        <button
-          type="submit"
-          disabled={ name.length < MIN_LENGTH }
-          data-testid="search-artist-button"
-        >
-          Lets Find It!
-        </button>
+      <div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div data-testid="page-search">
+            <Header />
+            <input
+              type="text"
+              name="name"
+              placeholder="eg Rolling Stones or Jack White"
+              onChange={ this.inputChange }
+              data-testid="search-artist-input"
+            />
+            <button
+              type="submit"
+              disabled={ name.length < MIN_LENGTH }
+              onClick={ this.searchBand }
+              data-testid="search-artist-button"
+            >
+              pesquisar
+            </button>
+            {searchList.length === 0 ? (
+              <span>Nenhum álbum foi encontrado</span>
+            ) : (
+              <section>
+                <div>{`Resultado de álbuns de: ${keepName}`}</div>
+                <div>
+                  {searchList.map((search) => (
+                    <div key={ search.collectionId }>
+                      <Link
+                        data-testid={ `link-to-album-${search.collectionId}` }
+                        to={ `/album/${search.collectionId}` }
+                      >
+                        <span>{search.collectionName}</span>
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              </section>
+            )}
+          </div>
+        )}
       </div>
     );
   }
