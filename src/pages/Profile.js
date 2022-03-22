a6003a866499622d083e92ef1794b07fe4fa21f4
Larissa Araujo <larissa.jvsa@gmail.com>
2022-03-22 20:16:39 -0300

    feat: :sparkles: add css on profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import ImgDefault from '../assets/css/img/imgDefault.png';
+import '../assets/css/Profile.css';
 
 export default class Profile extends Component {
   render() {
     return (
-      <div data-testid="page-profile">
+      <div className="container-profile-page" data-testid="page-profile">
         <Header />
+        <main className="container-profile">
+          <img className="profile-img" src={ ImgDefault } alt="default img" />
+          <p className="profile-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in
+            leo non nisl semper lobortis a vitae turpis. Fusce eget ante leo. In
+            volutpat elit vel libero aliquam, sit amet elementum leo commodo.
+            Cras dapibus finibus lectus vel luctus. Sed malesuada leo id dui
+            facilisis gravida.
+          </p>
+        </main>
       </div>
     );
   }
